6b0c16fd25a167fedd8c9f8dcdf92c3a04efe065
Marcelo Vargas <marcelo.vargas90@gmail.com>
2016-12-06 23:34:23 +0100

    Added integration test to delete books.

--- src/main/integration-test/library/controller/BookIntegrationTest.java
+++ src/main/integration-test/library/controller/BookIntegrationTest.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -15,8 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@PropertySource("classpath:application.properties")
-public class BookControllerIntegrationTest {
+public class BookIntegrationTest {
 
     private final String bookName = "Walking Dead: 1 Season";
 
@@ -43,6 +41,16 @@ public class BookControllerIntegrationTest {
         ResponseEntity<Book> bookResponse = this.restTemplate.getForEntity(String.format("/book/%d", addedBookResponseId), Book.class);
 
         assertThat(bookResponse.getBody().getId()).isEqualTo(addedBookResponseId);
+    }
+
+    @Test
+    public void deleteBook() throws Exception {
+        Book book = new Book(bookName);
+
+        ResponseEntity<Book> addedBookResponse = this.restTemplate.postForEntity("/book", book, Book.class);
+        Long addedBookResponseId = addedBookResponse.getBody().getId();
+
+        this.restTemplate.delete(String.format("/book/%d", addedBookResponseId));
     }
 
 }
